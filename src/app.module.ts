034b566bd35acfac98b1cf91129b9ca4a68c5b6d
dharaniEnactOn <dmraja@enacton.com>
2026-02-18 17:06:38 +0530

    feat(api telemetry module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { InventoryModule } from './inventory/inventory.module';
+import { TelemetryModule } from './telemetry/telemetry.module';
 
 @Module({
-  imports: [DatabaseModule, InventoryModule],
+  imports: [DatabaseModule, InventoryModule, TelemetryModule],
   controllers: [AppController],
   providers: [AppService],
 })
